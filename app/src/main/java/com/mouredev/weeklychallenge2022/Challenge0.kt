79c8fc15bc7fd125b839b2d5217aeff8ce124138
Anonimus93 <159485041+Anonimus93@users.noreply.github.com>
2024-02-08 16:00:12 -0400

    Update Challenge0.kt

--- app/src/main/java/com/mouredev/weeklychallenge2022/Challenge0.kt
+++ app/src/main/java/com/mouredev/weeklychallenge2022/Challenge0.kt
@@ -19,19 +19,12 @@ package com.mouredev.weeklychallenge2022
  *
  */
 
-fun main() {
-
-    for (index in 1..100) {
-        val divisibleByThree = index % 3 == 0
-        val divisibleByFive = index % 5 == 0
-        if (divisibleByThree && divisibleByFive) {
-            println("fizzbuzz")
-        } else if (divisibleByThree) {
-            println("fizz")
-        } else if (divisibleByFive) {
-            println("buzz")
-        } else {
-            println(index)
-        }
-    }
-}
+for num in range(1, 101):
+    if num % 3 == 0 and num % 5 == 0:
+        print("fizzbuzz")
+    elif num % 3 == 0:
+        print("fizz")
+    elif num % 5 == 0:
+        print("buzz")
+    else:
+        print(num)
